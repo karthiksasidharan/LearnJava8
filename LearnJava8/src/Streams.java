818f8f6dffd0f0ca5bca63dd8f309340a5d032cd
karthiksasidharan <karthiksasidharan6@gmail.com>
2015-09-20 11:07:24 +0530

    Add another

--- LearnJava8/src/Streams.java
+++ LearnJava8/src/Streams.java
@@ -8,6 +8,7 @@ public class Streams {
 		Item shirt = new Item(1, 1000.00, "Allen Solley");
 		Item pant = new Item(1, 500.00, "Levis");
 		Item shoe = new Item(1, 3000.00, "Nike");
+		Item shoe7 = new Item(1, 30000.00, "Nike");
 		List<Item> items = new ArrayList<Item>();
 		items.add(shoe);
 		items.add(pant);
